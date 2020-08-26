c3d471857f1aa0c0a0ea8e5e7c2dbc8938f761c2
Daniel Matichuk <dmatichuk@galois.com>
2020-08-26 14:19:18 -0700

    fix import for PPC test

--- tests/PPCTestMain.hs
+++ tests/PPCTestMain.hs
@@ -2,8 +2,10 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Main ( main ) where
+
 import qualified Pate.PPC as PPC
+import qualified Pate.Loader as PL
 import           TestBase
 
 main :: IO ()
-main = runTests "ppc" (ValidArchProxy @PPC.PPC64)
+main = runTests "ppc" (PL.ValidArchProxy @PPC.PPC64)
