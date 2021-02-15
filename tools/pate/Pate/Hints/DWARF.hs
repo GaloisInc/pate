3dbcd275cf427e32ecb2a5d7ac8a4df54f17b0de
Tristan Ravitch <tristan@ravit.ch>
2021-02-14 18:21:54 -0800

    Start a DWARF loader

--- tools/pate/Pate/Hints/DWARF.hs
+++ tools/pate/Pate/Hints/DWARF.hs
@@ -1,10 +1,35 @@
 module Pate.Hints.DWARF (
-  parseHints
+  parseDWARFHints,
+  DWARFError(..)
   ) where
 
 import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ElfEdit as DE
+import qualified Data.Dwarf as DD
 
 import qualified Pate.Hints as PH
 
-parseHints :: BSL.ByteString -> PH.VerificationHints
-parseHints = undefined
+data DWARFError = ELFParseError [DE.ElfParseError]
+                | ELFHeaderError Integer String
+  deriving (Show)
+
+traverseELFHints :: DE.Elf n -> (PH.VerificationHints, [DWARFError])
+traverseELFHints = undefined
+  where
+    lookupSection e b = DE.findSectionByName b e
+    -- dwarfSections = DD.mkSections
+
+parseDWARFHints :: BSL.ByteString -> (PH.VerificationHints, [DWARFError])
+parseDWARFHints bs0 =
+  case DE.parseElf (BSL.toStrict bs0) of
+    DE.Elf32Res errs e32 ->
+      let (hints, derrs) = traverseELFHints e32
+      in (hints, addELFErrors errs derrs)
+    DE.Elf64Res errs e64 ->
+      let (hints, derrs) = traverseELFHints e64
+      in (hints, addELFErrors errs derrs)
+    DE.ElfHeaderError off msg -> (mempty, [ELFHeaderError (toInteger off) msg])
+  where
+    addELFErrors elfErrs otherErrs
+      | null elfErrs = otherErrs
+      | otherwise = ELFParseError elfErrs : otherErrs
