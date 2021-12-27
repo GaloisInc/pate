5fdcdc1d636925249fb70212e6a4e7bc0fcecefe
Tristan Ravitch <tristan@ravit.ch>
2021-12-26 21:04:56 -0800

    Implement some of the statement handlers

--- src/Pate/Memory/Trace.hs
+++ src/Pate/Memory/Trace.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ViewPatterns #-}
 -- | A crucible extension for macaw IR using the trace memory model
 --
 -- This high-level interface is used during symbolic execution to implement the
@@ -15,6 +16,8 @@ module Pate.Memory.Trace (
   macawTraceExtensions
   ) where
 
+import           Control.Lens ( (^.), (&), (%~) )
+import           GHC.Stack ( HasCallStack )
 import           GHC.TypeLits ( type (<=) )
 import qualified What4.Interface as WI
 
@@ -24,9 +27,11 @@ import qualified Lang.Crucible.Backend as LCB
 import qualified Lang.Crucible.LLVM.MemModel as LCLM
 import qualified Lang.Crucible.Simulator as LCS
 import qualified Lang.Crucible.Simulator.ExecutionTree as LCSE
+import qualified Lang.Crucible.Simulator.GlobalState as LCSG
 
 import qualified Pate.Memory.Trace.MemTrace as PMTM
 import qualified Pate.Memory.Trace.Undefined as PMTU
+import qualified Pate.Panic as PP
 
 doPtrToBits
   :: ( LCB.IsSymInterface sym
@@ -70,23 +75,67 @@ evalMacawExpr undefPtrOps sym intrinsics logFn crucState f expr =
     DMS.PtrToBits _w x -> doPtrToBits undefPtrOps sym =<< f x
     _ -> DMS.evalMacawExprExtension sym intrinsics logFn crucState f expr
 
+getGlobalVar
+  :: (HasCallStack)
+  => LCS.CrucibleState s sym ext rtp blocks r ctx
+  -> LCS.GlobalVar mem
+  -> IO (LCS.RegValue sym mem)
+getGlobalVar cst gv =
+  case LCSG.lookupGlobal gv (cst ^. LCSE.stateTree . LCSE.actFrame . LCSE.gpGlobals) of
+    Just val -> return val
+    Nothing -> PP.panic PP.MemoryModel "getGlobalVar" ["Missing expected global variable: " ++ show gv]
+
+setGlobalVar
+  :: LCS.CrucibleState s sym ext rtp blocks r ctx
+  -> LCS.GlobalVar mem
+  -> LCS.RegValue sym mem
+  -> LCS.CrucibleState s sym ext rtp blocks r ctx
+setGlobalVar cst gv val = cst & LCSE.stateTree . LCSE.actFrame . LCSE.gpGlobals %~ LCSG.insertGlobal gv val
+
+-- | The custom evaluation function for macaw statements
+--
+-- We need to override most of these, but especially the memory model value
 execMacawStmt
   :: ( LCB.IsSymInterface sym
      , DMS.SymArchConstraints arch
+     , HasCallStack
      )
   => DMS.MacawArchEvalFn sym (PMTM.MemoryTrace arch) arch
   -> PMTU.UndefinedPtrOps sym
+  -> LCS.GlobalVar (PMTM.MemoryTrace arch)
   -> DMSB.MacawEvalStmtFunc (DMS.MacawStmtExtension arch) (DMS.MacawSimulatorState sym) sym (DMS.MacawExt arch)
-execMacawStmt = undefined
+execMacawStmt (DMSB.MacawArchEvalFn archStmtFn) undefPtrOps memVar stmt crucState =
+  case stmt of
+    DMS.MacawReadMem _addrWidth memRepr (LCS.regValue -> addr) -> do
+      memImpl0 <- getGlobalVar crucState memVar
+      let sym = crucState ^. LCSE.stateSymInterface
+      (res, memImpl1) <- PMTM.readMemory sym addr memRepr memImpl0
+      return (res, setGlobalVar crucState memVar memImpl1)
+
+    DMS.MacawCondReadMem _addrWidth memRepr (LCS.regValue -> cond) (LCS.regValue -> addr) (LCS.regValue -> def) -> do
+      memImpl0 <- getGlobalVar crucState memVar
+      let sym = crucState ^. LCSE.stateSymInterface
+      (res, memImpl1) <- PMTM.readMemoryConditional sym cond addr memRepr def memImpl0
+      return (res, setGlobalVar crucState memVar memImpl1)
+
+
+    DMS.MacawArchStateUpdate {} -> return ((), crucState)
+    DMS.MacawInstructionStart {} -> return ((), crucState)
+    DMS.MacawLookupFunctionHandle {} ->
+      PP.panic PP.MemoryModel "execMacawStmt" ["Function calls are not supported in the compositional verification extension"]
+    DMS.MacawLookupSyscallHandle {} ->
+      PP.panic PP.MemoryModel "execMacawStmt" ["System calls are not supported in the compositional verification extension"]
 
 macawTraceExtensions
   :: ( LCB.IsSymInterface sym
      , DMS.SymArchConstraints arch
+     , HasCallStack
      )
   => DMS.MacawArchEvalFn sym (PMTM.MemoryTrace arch) arch
   -> PMTU.UndefinedPtrOps sym
+  -> LCS.GlobalVar (PMTM.MemoryTrace arch)
   -> LCSE.ExtensionImpl (DMS.MacawSimulatorState sym) sym (DMS.MacawExt arch)
-macawTraceExtensions archEval undefPtrOps =
+macawTraceExtensions archEval undefPtrOps memVar =
   LCSE.ExtensionImpl { LCSE.extensionEval = evalMacawExpr undefPtrOps
-                     , LCSE.extensionExec = execMacawStmt archEval undefPtrOps
+                     , LCSE.extensionExec = execMacawStmt archEval undefPtrOps memVar
                      }
