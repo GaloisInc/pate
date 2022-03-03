9fa3eb469b3456205145c38723424663088f27a1
Daniel Matichuk <dmatichuk@galois.com>
2022-03-02 17:43:59 -0800

    cleanup docs for Pate.Ground

--- src/Pate/Ground.hs
+++ src/Pate/Ground.hs
@@ -48,18 +48,12 @@ module Pate.Ground
   ) where
 
 import           Data.Proxy
-import           Data.Map ( Map )
-import qualified Data.Map as Map
 import           Numeric.Natural ( Natural )
 import qualified Data.Kind as DK
-import qualified Control.Monad.IO.Class as IO
 import qualified Data.BitVector.Sized as BV
 
-import           Data.Parameterized.Some
 import qualified Data.Parameterized.Map as MapF
-import qualified Data.Parameterized.TraversableFC as FC
 import qualified Data.Parameterized.TraversableF as TF
-import           Data.Parameterized.Context (EmptyCtx, (::>), pattern Empty, pattern (:>))
 import qualified Data.Parameterized.Context as Ctx
 
 import qualified Data.Macaw.Symbolic as MS
@@ -70,7 +64,6 @@ import qualified Pate.ExprMappable as PEM
 import qualified Pate.Memory.MemTrace as MT
 import qualified Pate.Solver as PS
 
-import qualified What4.Concrete as W4C
 import qualified What4.Expr.GroundEval as W4G
 import qualified What4.Interface as W4
 import qualified What4.Partial as W4P
@@ -78,19 +71,23 @@ import qualified What4.ExprHelpers as WEH
 
 import qualified Pate.Panic as PP
 
--- | This module provides the 'Grounded' datatype as a simple wrapper which
--- existentially quantifies the 'sym' parameter for another type. This inner type
--- may contain symbolic what4 expressions internally, but with the intention that
--- they are all fully concrete values (i.e. asConcrete should be a total function for
--- any inner expression)
+-- | This module allows a model from What4 to be captured with respect to
+-- some expression-containing type. A 'Grounded' value contains a binding
+-- environment which is sufficient to ground any of the inner expressions of
+-- the type via 'groundValue'. Here an "inner" expression is defined by the
+-- traversal of the type according to 'PEM.ExprMappable'.
 
--- | A wrapper indicating that the inner expressions of
--- a datatype are fully concrete values
+-- | A value wrapped with a grounding environment, sufficient to
+-- concretely evaluate its inner expressions.
+-- The 'sym' parameter for the type is existentially quantified, and exposed
+-- through 'withGroundSym', indicating that any expressions parameterized over
+-- this new 'sym' parameter can necessarily be grounded with 'groundValue'.
 data Grounded (a :: sym -> DK.Type) where
   Grounded ::
     { _grndVal :: a sym
     , _grndData :: GroundData sym
     } -> Grounded a
+
 
 withGroundSym ::
   Grounded tp ->
@@ -110,21 +107,23 @@ instance TF.FoldableF Grounded where
 instance TF.TraversableF Grounded where
   traverseF f (Grounded v d) = Grounded <$> f v <*> pure d
 
--- | Ground values may be 'tagged' with additional metadata that may be lost
--- when grounding. Currently this tracks when a value may have been derived from
--- "undefined" pointer operations, which is lost during grounding, as those undefined
--- operations are represented as uninterpreted functions (which are necessarily concretely
--- defined when grounding).
+-- | Storage for the ground value of a term, including metadata.
 data GroundInfo (tp :: W4.BaseType) where
   GroundInfo ::
-    { groundPtrTag :: MT.UndefPtrOpTags
+    { -- | This can only be computed from the symbolic term with respect to
+      -- a specific model (see 'Pate.CounterExample.getPointerTags'),
+      -- and so it needs to be captured in the ground environment.
+      groundPtrTag :: MT.UndefPtrOpTags
     , groundVal :: W4G.GroundValue tp
     } -> GroundInfo tp
 
-
+-- | A binding environment representing the ground values for the inner expressions
+-- of some datatype.
 data GroundData sym where
   GroundData :: PS.ValidSym sym =>
     { grndSym :: sym
+    -- | This map is intended to be total with respect to every inner expression present
+    -- in the outer 'Grounded'
     , grndInfoMap :: MapF.MapF (W4.SymExpr sym) GroundInfo
     , grndStackRegion :: Natural
     } -> GroundData sym
@@ -178,6 +177,10 @@ integerToNat i
   | i >= 0 = fromIntegral i
   | otherwise = 0
 
+-- | Compute a grounding environment for a 'PEM.ExprMappable' value by evaluating
+-- each inner expression according to the given function. The value is unmodified
+-- in the resulting 'Grounded', but is paired with a grounding environment that is
+-- now necessarily sufficient to ground any of its inner expressions.
 ground ::
   forall sym a.
   PS.ValidSym sym =>
@@ -185,6 +188,7 @@ ground ::
   sym ->
   -- | stack region
   W4.SymNat sym ->
+  -- | Grounding inner expressions
   (forall tp. W4.SymExpr sym tp -> IO (GroundInfo tp)) ->
   a sym ->
   IO (Grounded a)
@@ -208,6 +212,3 @@ ground sym stackRegion mkinfo a = do
 -- by expression maps
 instance PEM.ExprMappable sym (Grounded a) where
   mapExpr _sym _f = return
-
-
--- concrete representations f
