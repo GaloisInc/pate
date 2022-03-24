83b6683c119010c19d8c27768ec512278ceedb13
Tristan Ravitch <tristan@ravit.ch>
2022-03-24 13:08:04 -0700

    Add some documentation on the CI system
    Includes a fix for a typo in the freeze file generator.
    
    [skip ci] because none of this is actually touched by the CI system

--- scripts/regenerate-freeze-files.sh
+++ scripts/regenerate-freeze-files.sh
@@ -5,9 +5,9 @@
 cabal freeze -w ghc-8.8.4
 mv cabal.project.freeze cabal.GHC-8.8.4.freeze
 
-cabal freeze -w 8.10.7
+cabal freeze -w ghc-8.10.7
 mv cabal.project.freeze cabal.GHC-8.10.7.freeze
 
 # We need allow-newer here because snap has lagging dependencies
-cabal freeze -w 9.0.2 --allow-newer=base
+cabal freeze -w ghc-9.0.2 --allow-newer=base
 mv cabal.project.freeze cabal.GHC-9.0.2.freeze
