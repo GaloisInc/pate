6fa0e855301e5f21db18a351e2f7e94a0d073b1a
Daniel Matichuk <dmatichuk@galois.com>
2023-03-14 23:31:49 -0700

    cleanup demo 5

--- demos/demo5/demo5.original.c
+++ demos/demo5/demo5.original.c
@@ -9,9 +9,9 @@ unsigned int mymax(unsigned int i, unsigned int j) {
 void demo5(unsigned int x, unsigned int y){
   unsigned int z = mymax(x,y);
   // 0x827c
-  if (z == x) { puts("First\n"); /* 0x8298 */ }
-  else if (z == y) { puts("Second\n"); /* 0x82b4 */ }
-  else { puts("Error\n"); /* 0x82c0 */ }
+  if (z == x) { puts("First"); /* 0x8298 */ }
+  else if (z == y) { puts("Second"); /* 0x82b4 */ }
+  else { puts("Error"); /* 0x82c0 */ }
 }
 
 int main(int argc, char *argv[]) {
