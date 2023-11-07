7ab3bfa76c4176ed6e4324769a823995f86e251d
Daniel Matichuk <dmatichuk@galois.com>
2023-11-07 13:31:29 -0800

    Fix incorrect handling of equivalence domain refinements
    Locations should have their predicate set to be unconditionally
    true in order to be included

--- src/Pate/Verification/Widening.hs
+++ src/Pate/Verification/Widening.hs
@@ -522,10 +522,7 @@ domainToEquivCondition scope bundle preD postD refine = withSym $ \sym -> do
   postD_eq' <- PL.traverseLocation @sym @arch sym (PAD.absDomEq postD) $ \loc p -> case refine loc of
     False -> return (PL.getLoc loc, p)
     -- modify postdomain to unconditionally include target locations
-    True -> do
-      case loc of
-        PL.Cell{} -> return $ (PL.getLoc loc, W4.falsePred sym)
-        _ -> return $ (PL.getLoc loc, W4.falsePred sym)
+    True -> return $ (PL.getLoc loc, W4.truePred sym)
 
   eqCtx <- equivalenceContext
   eqCond <- liftIO $ PEq.getPostdomain sym scope bundle eqCtx (PAD.absDomEq preD) postD_eq'
