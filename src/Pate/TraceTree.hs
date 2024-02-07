e57ecfb6b8623c9c5fd689a96d3814984ca62bd4
Daniel Matichuk <dmatichuk@galois.com>
2024-02-07 10:14:30 -0800

    Pate.Script.withIOList: reduce delay when re-checking for new results
    This fixes an issue with the Repl where the 'Choose Entry Point' prompt
    would be shown before the script made an entry point selection, making
    it appear that the verifier was blocked waiting for input.
    
    Strictly speaking the delay in this function shouldn't be necessary, since
    the execution is blocked on updates to the underlying list. However
    it is possible for the continuation to update the underlying list
    and therefore introduce a tight loop, which this delay would mitigate.

--- src/Pate/TraceTree.hs
+++ src/Pate/TraceTree.hs
@@ -242,7 +242,7 @@ withIOList (IOList ref mv) f = go
         -- once we finish
         Just b -> tryPutMVar mv () >> (return $ Right b)
         Nothing | isFinished st -> tryPutMVar mv () >> (return $ Left l)
-        Nothing -> threadDelay 1000000 >> go
+        Nothing -> threadDelay 10000 >> go
 
 mkStaticIOList :: [a] -> IO (IOList a)
 mkStaticIOList xs = do
