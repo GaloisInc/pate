36dd42cd17fd1e884d39908e2ae6eb601247f469
Daniel Matichuk <dmatichuk@galois.com>
2024-02-08 14:05:34 -0800

    mark desync-defer and desync-simple tests as expected success

--- tests/AArch32TestMain.hs
+++ tests/AArch32TestMain.hs
@@ -9,9 +9,8 @@ main = do
         { testArchName = "aarch32"
         , testArchLoader = AArch32.archLoader
         , testExpectEquivalenceFailure =
-            [ "stack-struct", "unequal/stack-struct", "max-signed",
+            [ "stack-struct", "unequal/stack-struct", "max-signed"
               -- missing interactive test support
-              "desync-defer", "desync-simple"
             ]
         , testExpectSelfEquivalenceFailure = []
         -- TODO: we should define a section name here and read its address
