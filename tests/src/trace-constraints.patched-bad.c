100760a83a42cf3581b9295d31b261d676d1e937
Daniel Matichuk <dmatichuk@galois.com>
2024-08-13 14:04:44 -0700

    add trace-constraints test

---
+++ tests/src/trace-constraints.patched-bad.c
@@ -0,0 +1,20 @@
+#include "util.h"
+
+void _start();
+
+int h;
+int f;
+int i;
+int dump;
+int g __attribute__((section(".output"))) = -12;
+int j __attribute__((section(".output"))) = -12;
+
+void _start() {
+  dump = h;
+  dump = i;
+
+  if (h == 1){
+    g = f;
+  }
+  j = i;
+}
